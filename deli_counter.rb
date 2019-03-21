f31a306f392eb9f570a5b54ef9e6255093c54885
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-21 01:52:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(numCustomers)
+  count = 0
+  if numCustomers == 1
+    puts 'The line is currently empty.'
+  end
+  else
+    puts 'The line is currently: '
+    numCustomers.each {|name| do
+      puts count + '. ' + name + ' '
+      count += 1}
+    end
+end
